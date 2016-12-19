af6c7958788a23421e89f917e64d5744bb0ae143
Stronnics <paiva.santiago@gmail.com>
2016-12-19 15:48:14 -0500

    add new queries

--- uploadNeuroDB/image_insert.pl
+++ uploadNeuroDB/image_insert.pl
@@ -27,15 +27,19 @@ my $debug       = 0;
 my $profile = undef;
 my $verbose    = 0;
 my $file;
+my $filename;
 my $file_extension;
 my $type;
 my $sth;
 my $InsertedByUserID = undef;
 my $sessionID = undef;
 my $patient_name = undef;
-my $session_id = undef;
+my $SessionID = undef;
 my $TarchiveSource = undef;
-
+my $script;
+my $FileID = undef;
+my $CandID = undef;
+my $checkPicID = undef;
 
 ################################################################
 #### These settings are in a config file (profile) #############
@@ -73,17 +77,17 @@ if(scalar(@ARGV) != 2) {
 
 ################################################################
 ############ Populate main parameters ##########################
-###############################################################
+################################################################
 
 # We get the name of the file in the path provided 
-$file = basename(abs_path($ARGV[0]));
+$file = $ARGV[0];
 $patient_name = $ARGV[1];
 
-# TODO: refactor this. 
-# We get the extension of the file
-($file_extension) = $file =~ /(\.[^.]+)$/;
 
-$type = substr $file_extension, 1;
+# We get the filename, basename, and extension of the iamge file
+my ($basename, $parentdir, $extension) = fileparse($file, qr/\.[^.]*$/);
+$filename = $basename . $extension;
+$type = substr $extension, 1; # Remove the dot from the extension
 
 # We get the user that is performing the upload
 $InsertedByUserID = `whoami`;
@@ -91,46 +95,93 @@ $InsertedByUserID = `whoami`;
 ################################################################
 ############### Establish database connection ##################
 ################################################################
+print "\n Connecting to database \n" if $verbose;
 
 my $dbh = &NeuroDB::DBI::connect_to_db(@Settings::db);
-
 
 ################################################################
 ############## We find the SessionID ###########################
 ################################################################
+print "\n Getting the SessionID \n" if $verbose;
 
 $sth = $dbh->prepare("SELECT SessionID FROM tarchive WHERE PatientName=?");
 my @params = ($patient_name);
 $sth->execute(@params); 
-$session_id = $sth->fetchrow_array();
+$SessionID = $sth->fetchrow_array();
 
 ################################################################
-############### Create Insert Query ############################
+############### Insert into files table ########################
 ################################################################
-print "\nInserting into Files table\n" if $verbose;
-
-#TODO: TarchiveSource value is missing. Need to get it from tarchive table 
+print "\n Inserting into Files table\n" if $verbose;
 
 # We build the insert query
 my $query = $dbh->prepare("INSERT INTO files (File, SessionID, FileType, InsertedByUserID, InsertTime, SourceFileID,TarchiveSource) VALUES (?,?,?,?,?,?,?)"); 
 
 # Query parameters     
-my @results = ($file,$session_id,$type,$InsertedByUserID,time,undef);
+my @results = ($filename,$SessionID,$type,$InsertedByUserID,time,undef);
 
 # Run query
 #$query->execute(@results);
 
+################################################################
+############# Get FileID  ######################################
+################################################################
+print "\n Getting FileID \n" if $verbose;
 
-############################################################
-############# Create minc-pics #############################
-############################################################
-print "\nCreating Minc Pics\n" if $verbose;
+$sth = $dbh->prepare("SELECT FileID from files where File=?");
+@params = ($filename);
+$sth->execute(@params);
+$FileID = $sth->fetchrow_array();
 
-&NeuroDB::MRI::make_minc_pics(\$dbh,
-                              $TarchiveSource,
-                              $profile,
-                              0, # minFileID $row[0], maxFileID $row[1]
-                              $debug, 
-                              $verbose);
+#print "FileID is " . $FileID . "\n"; 
+
+###############################################################
+############# Get CandID ######################################
+###############################################################
+print "\n Getting CandID \n" if $verbose;
+ 
+$sth = $dbh->prepare("SELECT CandID from session where ID=?");
+@params = ($SessionID);
+$sth->execute(@params);
+$CandID = $sth->fetchrow_array();
+
+#print "CandID is " . "$CandID" . "\n";
+
+###############################################################
+############# Get CheckPicID reference ########################
+###############################################################
+print "\n Getting the CheckPicID reference number \n" if $verbose;
+
+$sth = $dbh->prepare("SELECT \@checkPicID:=ParameterTypeID FROM parameter_type WHERE Name='check_pic_filename'");
+$sth->execute();
+$checkPicID = $sth->fetchrow_array();
+
+#print "CheckPicID is " . "$checkPicID" . "\n";
+
+###############################################################
+############# Insert into parameter_file table ################
+###############################################################
+print "\n Inserting into parameter_file table \n" if $verbose;
+
+$sth = $dbh->prepare("INSERT INTO parameter_file (FileID, ParameterTypeID, Value, InsertTime) VALUES (?,?,?,?)");
+my $value = "$CandID" . "/" . uc("$basename") . uc("_$type") . "_$FileID" . "_check.jpg"; 
+@params =  ($FileID,$checkPicID,$value,time); 
+#$sth->execute(@params);
+
+# print "Value is " . "$value" . "\n";
+
+###############################################################
+############# Calling Mass_Pic ################################
+###############################################################
+print "\n Calling mass_pic \n" if $verbose;
+
+$script = "./mass_pic.pl -minFileID $FileID -maxFileID $FileID -profile $profile";
+
+if ($verbose) {		
+   $script .= " -verbose";		
+}
+
+system($script);
+
 
 exit;
